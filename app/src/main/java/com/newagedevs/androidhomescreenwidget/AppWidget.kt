cdb833d16d8fb34a5cb10aed43b295df442bb9fd
imamhossain94 <imamagun94@gmail.com>
2021-07-23 06:50:20 +0600

    feat: update service added.

--- app/src/main/java/com/newagedevs/androidhomescreenwidget/AppWidget.kt
+++ app/src/main/java/com/newagedevs/androidhomescreenwidget/AppWidget.kt
@@ -29,7 +29,7 @@ internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManage
     val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.app_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    views.setTextViewText(R.id.textView, widgetText)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
---
+++ app/src/main/java/com/newagedevs/androidhomescreenwidget/UpdateService.kt
@@ -0,0 +1,31 @@
+package com.newagedevs.androidhomescreenwidget
+
+import android.app.Service
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
+import android.content.Intent
+import android.os.IBinder
+import android.widget.RemoteViews
+import java.util.*
+
+
+class UpdateService : Service() {
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        // generates random number
+        val random = Random()
+        val randomInt = random.nextInt(100)
+        val lastUpdate = "R: $randomInt"
+        // Reaches the view on widget and displays the number
+        val view = RemoteViews(packageName, R.layout.app_widget)
+        view.setTextViewText(R.id.textView, lastUpdate)
+        val theWidget = ComponentName(this, AppWidget::class.java)
+        val manager = AppWidgetManager.getInstance(this)
+        manager.updateAppWidget(theWidget, view)
+        return super.onStartCommand(intent, flags, startId)
+    }
+}
